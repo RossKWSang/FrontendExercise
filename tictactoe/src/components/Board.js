71c52df0fd4509f65af1f6a07e20a0d45cca287d
Wonsang Kim <rosskysoss@gmail.com>
2024-06-12 20:35:55 +0900

    state

--- tictactoe/src/components/Board.js
+++ tictactoe/src/components/Board.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
 import Square from './Square'
+import "./Board.css"
 
 export default class Board extends Component {
+
+    // super 키워드는 자식 클래스 내에서 부모 클래스의 생성자를 호출할 때 사용됨
+    // super 키워드는 자식 클래스 내에서 부모 클래스의 메서드를 호출할 때 사용됨
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({ squares: squares });
+    }
+
     renderSquare(i) {
-        return <Square />
+        return <Square value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)} />;
     }
 
     render() {
@@ -26,6 +45,6 @@ export default class Board extends Component {
                     {this.renderSquare(8)}
                 </div>
             </div>
-        )
+        );
     }
 }
