661ae6d92579c12b5e3c6da2edc2d228ec0d5494
Wonsang Kim <rosskysoss@gmail.com>
2024-06-13 18:01:26 +0900

    jump to

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,15 +1,85 @@
 import "./App.css"
 import Board from "./components/Board";
+import { useState } from 'react'
 
 
-function App() {
+const App = () => {
+
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setxIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let index = 0; index < lines.length; ++index) {
+      const [a, b, c] = lines[index];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+
+  let status;
+
+  if (winner) {
+    status = `Winner is ${winner}`;
+  } else {
+    status = `Next player ${xIsNext ? 'X' : 'O'}`;
+  }
+
+  const handleClick = (i) => {
+    const newSquares = current.squares.slice();
+
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setHistory([...history, { squares: newSquares }]);
+    setxIsNext(prev => !prev);
+  }
+
+  const moves = history.map((step, move) => {
+    const describe = move ?
+      `Go to move #${move}` :
+      `Go to game start`;
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{describe}</button>
+      </li>
+    );
+  })
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setxIsNext(step % 2 === 0);
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board
+          squares={current.squares}
+          onClick={(i) => handleClick(i)}
+        />
       </div>
       <div className="game-info">
-        game-info
+        <div className="status">{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
--- tictactoe/src/components/Board.js
+++ tictactoe/src/components/Board.js
@@ -1,50 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Square from './Square'
 import "./Board.css"
 
-export default class Board extends Component {
+// export default class Board extends Component {
 
+const Board = ({ squares, onClick }) => {
     // super 키워드는 자식 클래스 내에서 부모 클래스의 생성자를 호출할 때 사용됨
     // super 키워드는 자식 클래스 내에서 부모 클래스의 메서드를 호출할 때 사용됨
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-        };
+    // const [getter, setter]
+
+    const renderSquare = (i) => {
+        return <Square value={squares[i]}
+            onClick={() => onClick(i)} />;
     }
 
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({ squares: squares });
-    }
-
-    renderSquare(i) {
-        return <Square value={this.state.squares[i]}
-            onClick={() => this.handleClick(i)} />;
-    }
-
-    render() {
-        return (
-            <div>
-                <div className='status'>Next Player: X, O</div>
-                <div className='board-row'>
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className='board-row'>
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className='board-row'>
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+    return (
+        <div>
+            <div className='board-row'>
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
-        );
-    }
+            <div className='board-row'>
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className='board-row'>
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
 }
+
+export default Board;
