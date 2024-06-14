27357a8c2629c091ac971be341d71955312b8f64
Wonsang Kim <rosskysoss@gmail.com>
2024-06-14 17:53:30 +0900

    commit

---
+++ react-disney-plus-app/src/components/Banner.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import axios from '../api/axios'
+import requests from '../api/request'
+
+const Banner = () => {
+
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const response = await axios.get(requests.fetchNowPlaying)
+        const movieId =
+            response.data.results[
+                Math.floor(Math.random() * requests.data.results.length)
+            ].id;
+
+        const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+            params: { append_to_response: "videos" },
+        })
+        setMovie(movieDetail)
+    }
+
+
+    return (
+        <div>Banner</div>
+    )
+}
+
+export default Banner
