402c3de6b8d69ac97b8cd1075425666613a5f686
Wonsang Kim <rosskysoss@gmail.com>
2024-06-25 17:09:04 +0900

    commit

--- react-disney-plus-app/src/hooks/useOnClickOutside.js
+++ react-disney-plus-app/src/hooks/useOnClickOutside.js
@@ -17,5 +17,5 @@ export default function useOnClickOutside(ref, handler) {
             document.removeEventListener("touchstart", listener);
         }
     }, [ref, handler])
-    // 오늘도 하긴 했다.
+    // 리액트는 더 어렵다.
 }
